68d309dbeb6c848bf898657ad662ad51864e0991
s79ns <67348810+s79ns@users.noreply.github.com>
2020-12-06 15:20:48 +0900

    スクロールでふんわりアニメーションを追加

--- app/javascript/packs/scroll.js
+++ app/javascript/packs/scroll.js
@@ -30,3 +30,16 @@ document.addEventListener("click", (e) => {
     block: "start",
   });
 });
+
+// コンテンツをスクロールするとふわっと出てくる
+
+const targetElement = document.querySelectorAll(".animationTarget");
+
+document.addEventListener("scroll", function () {
+  for (let i = 0; i < targetElement.length; i++) {
+    const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * 0.6;
+    if (window.innerHeight > getElementDistance) {
+      targetElement[i].classList.add("show");
+    }
+  }
+});
