4e3b3826bcc06291ab1e3260104e413ad32539c7
s79ns <67348810+s79ns@users.noreply.github.com>
2020-12-10 10:55:13 +0900

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,12 @@ schedule_file = "config/schedule.yml"
 
 Sidekiq.configure_server do |config|
   if Rails.env.production?
-    config.redis = { url: ENV.fetch('p56e546a735ce4a1988aacc2c77f743daae7b6f6a4351395fe40c841c23a8d4ab@ec2-34-192-46-42.compute-1.amazonaws.com:27079') }
+    config.redis = { url: ENV.fetch('REDIS_URL') }
   end
 end
 Sidekiq.configure_client do |config|
   if Rails.env.production?
-    config.redis = { url: ENV.fetch('p56e546a735ce4a1988aacc2c77f743daae7b6f6a4351395fe40c841c23a8d4ab@ec2-34-192-46-42.compute-1.amazonaws.com:27079') }
+    config.redis = { url: ENV.fetch('REDIS_URL') }
   end
 end
 
