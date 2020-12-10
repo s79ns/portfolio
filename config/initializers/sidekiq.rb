281b87cfef56a20ee9aa10996fbfb99eeb7d0918
s79ns <67348810+s79ns@users.noreply.github.com>
2020-12-10 10:08:29 +0900

    heroku定期通信準備

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,16 @@
 schedule_file = "config/schedule.yml"
 
+Sidekiq.configure_server do |config|
+  if Rails.env.production?
+    config.redis = { url: ENV.fetch('redis://:p56e546a735ce4a1988aacc2c77f743daae7b6f6a4351395fe40c841c23a8d4ab@ec2-34-192-46-42.compute-1.amazonaws.com:27079') }
+  end
+end
+Sidekiq.configure_client do |config|
+  if Rails.env.production?
+    config.redis = { url: ENV.fetch('redis://:p56e546a735ce4a1988aacc2c77f743daae7b6f6a4351395fe40c841c23a8d4ab@ec2-34-192-46-42.compute-1.amazonaws.com:27079') }
+  end
+end
+
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
