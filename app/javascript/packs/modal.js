d9ad2774be549ba01808a331b096513f698fea00
s79ns <67348810+s79ns@users.noreply.github.com>
2020-12-13 09:40:40 +0900

    ファイル整理整頓

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -11,3 +11,4 @@ $(function () {
 });
 
 // 発火地点に(data-target="a")、表示させたいモーダルに#(data-targetで定義した値)をそれぞれ記述して使って下さい
+// Gemfile → gem 'jquery-rails'、application.html.haml → %script{src:"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"}
